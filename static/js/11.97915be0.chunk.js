9a80d4040ace7fa4621eeb57993e2d69d0ecfd55
andriipanekalo <andrew.panekalo@gmail.com>
2022-01-08 18:26:07 +0200

    Updates

--- static/js/11.97915be0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpnewspaper=this.webpackJsonpnewspaper||[]).push([[11],{331:function(n,r,e){"use strict";function t(){return"Error"}e.r(r),e.d(r,"default",(function(){return t}))}}]);
-//# sourceMappingURL=11.97915be0.chunk.js.map
